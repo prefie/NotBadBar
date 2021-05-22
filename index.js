a69a1af33c01328d26a462f068d2846862faf200
vtarasovaaa <vtarasova394@gmail.com>
2021-05-22 17:56:15 +0500

    Подключила handlebars

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-import express from 'express'
-import path from 'path'
+import express from 'express';
+import path from 'path';
+import hbs from "express-handlebars";
 
-const __dirname = path.resolve()
 const port = process.env.PORT ?? 3000;
 const app = express();
+const rootDir = process.cwd();
 
-app.use(express.static(path.resolve(__dirname, 'visualisation')))
+app.set("view engine", "hbs");
+
+app.engine(
+    "hbs",
+    hbs({
+        extname: "hbs",
+        defaultView: "default",
+        layoutsDir: path.join(rootDir, "/views/layouts/"),
+    })
+);
+
+app.use('/visualisation', express.static('visualisation'));
+
+app.get("/", (_, res) => {
+    res.redirect('/main');
+});
+
+app.get('/game', (req, res) => {
+    res.render('game');
+});
+
+app.get('/main', (req, res) => {
+    res.render('start');
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}...`));
-
